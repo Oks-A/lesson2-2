cc6e8c23ecfd81c80b4b1efeffc5dc78b3d9d973
Oksana <oksanka-bud@rambler.ru>
2023-11-07 22:31:25 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int prise = 10_000; // цена билета
+        int miles = 20;   // колличество милей
+        System.out.println(prise / miles); // цену билета делим на бонусные мили
+
+
     }
 }
 
-.idea
-*.iml
-out/
+
+
+
